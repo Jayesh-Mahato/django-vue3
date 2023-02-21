edf62b6b7cd9d3f5df7763e1898b985ac270f869
Jayesh <jayesh@virtualinit.net>
2023-02-21 22:13:20 +0530

    first commit

---
+++ src/cfehome/context_processors.py
@@ -0,0 +1,12 @@
+from django.conf import settings
+
+def vue_files(request):
+    static_base_dir = settings.STATIC_BASE_DIR
+    vue_name = 'vue-prod' if settings.VUE_PROD else 'vue-dev'
+    vue_dir = static_base_dir / vue_name
+    js_files = [x.relative_to(static_base_dir) for x in vue_dir.glob("**/*.js")]
+    css_files = [x.relative_to(static_base_dir) for x in vue_dir.glob("**/*.css")]
+    return {
+        "vue_js_paths": list(js_files),
+        "vue_css_paths": list(css_files)
+    }
